c72711b67623106616a7a4da19ce717aa616af56
Sunny9011 <volokonskiy52@gmail.com>
2020-06-03 08:25:22 +0300

    Type Hinting

--- dataBase/XmlFeedModel.php
+++ dataBase/XmlFeedModel.php
@@ -10,29 +10,29 @@ class XmlFeedModel
     protected $pubDate;
     protected $query;
 
-    public function saveData($connect)
+    public function saveData(DataBase $connect): void
     {
             $this->query = "INSERT INTO xmlfeed (link, title, description, pubDate)
     VALUES ( '" . $this->link . "', '" . $this->title . "', '" . $this->description . "', '" . $this->pubDate . "')";
         $connect->connect()->exec($this->query);
     }
 
-     public function setLink($value)
+     public function setLink(string $value): string
     {
         $this->link = $value;
     }
 
-    public function setTitle($value)
+    public function setTitle(string $value): string
     {
         $this->title = $value;
     }
 
-    public function setDescription($value)
+    public function setDescription(string $value): string
     {
         $this->description = $value;
     }
 
-    public function setPubDate($value)
+    public function setPubDate(string $value): string
     {
         $this->pubDate = $value;
     }
